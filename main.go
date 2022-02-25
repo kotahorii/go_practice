4d227dac7230c60d1a3d0061f6316221fb14ee8b
kotahorii <khorii.micron@gmail.com>
2022-02-25 14:56:07 +0900

    initial commit

--- main.go
+++ main.go
@@ -2,16 +2,38 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	fmt.Println(sqrt(2), sqrt(-4))
+	fmt.Println("counting")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+	fmt.Println("done")
 }
 
-func sqrt(x float64) string {
-	if x < 0 {
-		return sqrt(-x) + "i"
-	}
-	return fmt.Sprint(math.Sqrt(x))
-}
+// func sqrt(x float64) string {
+// 	if x < 0 {
+// 		return sqrt(-x) + "i"
+// 	}
+// 	return fmt.Sprint(math.Sqrt(x))
+// }
+
+// func pow(x, n, lim float64) float64 {
+// 	if v := math.Pow(x, n); v < lim {
+// 		return v
+// 	} else {
+// 		fmt.Printf("%g >= %g\n", v, lim)
+// 	}
+// 	return lim
+// }
+
+// func Sqrt(x float64) float64 {
+// 	z := 1.0
+// 	for y := 0.0; y <= 10; y++ {
+// 		z -= (z*z - y) / (2 * z)
+// 		fmt.Println(z / math.Sqrt(x))
+// 	}
+// 	return z
+// }
