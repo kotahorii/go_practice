59476ee393f60dcc3dffcfa032a8e8dca72107dc
kotahorii <khorii.micron@gmail.com>
2022-03-01 22:47:00 +0900

    update

--- main.go
+++ main.go
@@ -3,25 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	q := []int{2, 3, 5, 7, 11, 13}
-	fmt.Println(q)
-
-	r := []bool{true, false, true, true, false, true}
-	fmt.Println(r)
-
-	s := []struct {
-		i int
-		b bool
-	}{
-		{2, true},
-		{3, false},
-		{5, true},
-		{7, true},
-		{11, false},
-		{13, true},
+	var s []int
+	fmt.Println(s, len(s), cap(s))
+	if s == nil {
+		fmt.Println("nil!")
 	}
-	fmt.Println(s)
 }
+
+// func printSlice(s []int) {
+// 	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+// }
 
 // func sqrt(x float64) string {
 // 	if x < 0 {
