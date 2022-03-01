a91ecedfc73202b94fae7ab4c888eda5ae21440f
kotahorii <khorii.micron@gmail.com>
2022-03-01 22:30:55 +0900

    update

--- main.go
+++ main.go
@@ -3,17 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	var a [2]string
-	a[0] = "Hello"
-	a[1] = "World"
-	fmt.Println(a[0], a[1])
-	fmt.Println(a)
+	names := [4]string{
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
+	fmt.Println(names)
 
-	primes := [6]int{2, 3, 5, 7, 11, 13}
-	fmt.Println(primes)
+	a := names[0:2]
+	b := names[1:3]
+	fmt.Println(a, b)
 
-	var s []int = primes[1:4]
-	fmt.Println(s)
+	b[0] = "XXX"
+	fmt.Println(a, b)
+	fmt.Println(names)
 }
 
 // func sqrt(x float64) string {
