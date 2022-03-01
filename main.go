a8367a4038d58b74ea0b07e1078419f7483c60c1
kotahorii <khorii.micron@gmail.com>
2022-03-01 23:39:18 +0900

    update

--- main.go
+++ main.go
@@ -8,15 +8,14 @@ type Vertex struct {
 	Lat, Long float64
 }
 
-var m map[string]Vertex
-
 func main() {
-	m = make(map[string]Vertex)
-	m["Bell Labs"] = Vertex{
-		40.68433, -74.39964,
-	}
-	fmt.Println(m["Bell Labs"])
-	fmt.Println(m)
+	m := make(map[string]int)
+
+	m["Answer"] = 42
+	fmt.Println("The value:", m["Answer"])
+	delete(m, "Answer")
+	v, ok := m["Answer"]
+	fmt.Println("The value:", v, "Present?", ok)
 }
 
 func printSlice(x []int) {
