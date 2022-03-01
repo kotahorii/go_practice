0147502e160bcebfa071ea80a1194a875b09cf29
kotahorii <khorii.micron@gmail.com>
2022-03-01 23:30:17 +0900

    update

--- main.go
+++ main.go
@@ -1,18 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main() {
-	var s []int
-	fmt.Println(s, len(s), cap(s))
-	if s == nil {
-		fmt.Println("nil!")
-	}
+type Vertex struct {
+	Lat, Long float64
 }
 
-// func printSlice(s []int) {
-// 	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
-// }
+var m map[string]Vertex
+
+func main() {
+	m = make(map[string]Vertex)
+	m["Bell Labs"] = Vertex{
+		40.68433, -74.39964,
+	}
+	fmt.Println(m["Bell Labs"])
+	fmt.Println(m)
+}
+
+func printSlice(x []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(x), cap(x), x)
+}
 
 // func sqrt(x float64) string {
 // 	if x < 0 {
