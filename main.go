c920749d5f6222db2ec7362a5c1f48bf04b439e3
kotahorii <khorii.micron@gmail.com>
2022-03-03 13:26:00 +0900

    update

--- main.go
+++ main.go
@@ -2,20 +2,31 @@ package main
 
 import "fmt"
 
+// func do(i interface{}) {
+// 	switch v := i.(type) {
+// 	case int:
+// 		fmt.Printf("Twice %v is %v\n", v, v*2)
+// 	case string:
+// 		fmt.Printf("%q is %v bytes long\n", v, len(v))
+// 	default:
+// 		fmt.Printf("I don't know about type %T!\n", v)
+// 	}
+// }
+
+type IPAddr [4]byte
+
+func (i IPAddr) String() string {
+	return fmt.Sprintf("%v.%v.%v.%v", i[0], i[1], i[2], i[3])
+}
+
 func main() {
-	var i interface{} = "hello"
-
-	s := i.(string)
-	fmt.Println(s)
-
-	s, ok := i.(string)
-	fmt.Println(s, ok)
-
-	f, ok := i.(float64)
-	fmt.Println(f, ok)
-
-	f = i.(float64)
-	fmt.Println(f)
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
 }
 
 // func fibonacchi() func() int {
