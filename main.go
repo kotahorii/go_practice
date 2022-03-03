d3c15c8b2ef786024f9916d9e11649c91fc4476a
kotahorii <khorii.micron@gmail.com>
2022-03-03 12:44:54 +0900

    update

--- main.go
+++ main.go
@@ -9,28 +9,20 @@ type Vertex struct {
 	X, Y float64
 }
 
-func (v Vertex) Abs() float64 {
-	return math.Sqrt(v.X*v.X + v.Y*v.Y)
-}
-
-// type MyFloat float64
-
-// func (f MyFloat) Abs() float64 {
-// 	if f < 0 {
-// 		return float64(-f)
-// 	}
-// 	return float64(f)
-// }
-
 func (v *Vertex) Scale(f float64) {
 	v.X *= f
 	v.Y *= f
 }
 
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
 func main() {
-	v := Vertex{3, 4}
-	v.Scale(10)
-	fmt.Println(v.Abs())
+	v := &Vertex{3, 4}
+	fmt.Printf("Before scaling: %+v, Abs: %v\n", v, v.Abs())
+	v.Scale(5)
+	fmt.Printf("After scaling: %+v, Abs: %v\n", v, v.Abs())
 }
 
 // func fibonacchi() func() int {
