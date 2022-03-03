12698d0e349e88e2ce4f4146c1a297ed99849736
kotahorii <khorii.micron@gmail.com>
2022-03-03 13:12:18 +0900

    update

--- main.go
+++ main.go
@@ -1,41 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
-
-type I interface {
-	M()
-}
-
-type T struct {
-	S string
-}
-
-func (t *T) M() {
-	fmt.Println(t.S)
-}
-
-type F float64
-
-func (f F) M() {
-	fmt.Println(f)
-}
+import "fmt"
 
 func main() {
-	var i I
-	i = &T{"Hello"}
-	describe(i)
-	i.M()
+	var i interface{} = "hello"
 
-	i = F(math.Pi)
-	describe(i)
-	i.M()
-}
+	s := i.(string)
+	fmt.Println(s)
 
-func describe(i I) {
-	fmt.Printf("(%v, %T)\n", i, i)
+	s, ok := i.(string)
+	fmt.Println(s, ok)
+
+	f, ok := i.(float64)
+	fmt.Println(f, ok)
+
+	f = i.(float64)
+	fmt.Println(f)
 }
 
 // func fibonacchi() func() int {
